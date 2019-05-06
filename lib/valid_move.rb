0ce95301bdd388ef761a9251642dd5ad95ff345d
Bernadette Davis <bsd231@nyu.edu>
2019-05-06 19:35:23 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,15 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if !position_taken?(board, index)
+  if index < 9 && index > -1
+    if !position_taken?(board, index)
+      return true
+    else
+      return false
+    end
 
   else
+    return false
+
 
   end
 end
